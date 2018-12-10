590a9b737b996f6aec6a4a70d145e210bc7f3019
João Paulo <macieljp@outlook.com>
2018-12-10 03:11:51 -0200

    João Paulo

---
+++ scripts bd/script_popular_bd.sql
@@ -0,0 +1,21 @@
+INSERT INTO `apartamento` VALUES (3,2,'Luxo',12);
+
+ INSERT INTO `cliente` VALUES (1,'123131',NULL,'123sdfsf','asdf','1234','F',1);
+ 
+ INSERT INTO `cliente_telefone` VALUES (1,1),(1,2);
+ 
+ INSERT INTO `endereco` VALUES (2,'Centro','86310000','casa','Rua Goias',203,'Rua - R',2);
+ 
+ INSERT INTO `estado` VALUES(2,'Parana');
+ 
+ INSERT INTO `grupodeproduto` VALUES (1,'Bebidas');
+ 
+ INSERT INTO `hospedar` VALUES (2,'2019-01-01','2018-12-11',_binary '\0',3,1);
+ 
+ INSERT INTO `municipio` VALUES (1,'sp',1),(2,'Nova Fatima',2),(3,'Ubirata',3);
+ 
+ INSERT INTO `produto` VALUES (1,'Coca Cola','Refrigerante Coca Cola');
+ 
+ INSERT INTO `reserva` VALUES (1,'0018-12-05',NULL,'Concluida',NULL,NULL);
+ 
+ INSERT INTO `telefone` VALUES (1,'1231'),(2,'1231'),(3,'44999302584'),(4,'449444444444');
